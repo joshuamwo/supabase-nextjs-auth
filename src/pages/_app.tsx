9c593df36537656c4d44d0ceb4013a63d9978fa9
Yullu <joshuamuuo5@gmail.com>
2023-04-10 11:12:39 +0300

    added inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({
   Component,
@@ -16,8 +19,11 @@ function MyApp({
     <SessionContextProvider
       supabaseClient={supabase}
       initialSession={pageProps.initialSession}
-    >
+			>
+				<main className={inter.className}>
+
       <Component {...pageProps} />
+				</main>
     </SessionContextProvider>
   );
 }
