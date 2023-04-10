b7ada22410315f06857c6ced8c2539c223b9e29a
Yullu <joshuamuuo5@gmail.com>
2023-04-10 10:47:43 +0300

    webpack ignore fs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, { nextRuntime }) {
-    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
-    if (typeof nextRuntime === "undefined") {
-      const { IgnorePlugin } = require("webpack");
-      const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
-      config.plugins.push(ignoreFs);
-    }
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
 
     return config;
   },
