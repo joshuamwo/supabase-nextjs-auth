9b31055988487f53013f7e73d7c7f5f5915b8146
Yullu <joshuamuuo5@gmail.com>
2023-04-10 12:34:19 +0300

    added tailwind styling

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -80,48 +80,73 @@ export default function Account({ session }: { session: Session }) {
   }
 
   return (
-    <div className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="website"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
+    <div className="flex justify-center">
+      <form className=" flex flex-col items-center justify-center max-w-4xl px-4">
+        <div className="w-full px-3 mb-3">
+          <label
+            htmlFor="email"
+            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="text"
+            mb-3
+            value={session.user.email}
+            disabled
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+          />
+        </div>
+        <div className="w-full px-3 mb-3">
+          <label
+            htmlFor="username"
+            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+          >
+            Username
+          </label>
+          <input
+            id="username"
+            type="text"
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+          />
+        </div>
 
-      <div>
-        <button
-          className="button primary block"
-          onClick={() => updateProfile({ username, website, avatar_url })}
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-      </div>
+        <div className="w-full px-3 mb-8">
+          <label
+            htmlFor="website"
+            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+          >
+            Website
+          </label>
+          <input
+            id="website"
+            type="website"
+            value={website || ""}
+            onChange={(e) => setWebsite(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4leading-tight focus:outline-none focus:bg-white"
+          />
+        </div>
 
-      <div>
-        <button
-          className="button block"
-          onClick={() => supabase.auth.signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
+        <div className="w-full px-3">
+          <button
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-6 rounded"
+            onClick={() => updateProfile({ username, website, avatar_url })}
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
+          </button>
+
+          <button
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => supabase.auth.signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
