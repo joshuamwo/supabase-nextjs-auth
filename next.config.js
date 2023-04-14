8504ebb1cee839bf674d3114876d6ae03dfa7869
Yullu <joshuamuuo5@gmail.com>
2023-04-14 17:53:42 +0300

    basepath: /work/supabase-authui

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/work/supabase-authui/",
+  basePath: "/work/supabase-authui",
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
